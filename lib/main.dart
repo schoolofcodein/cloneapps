92e8ac4df444a207ec2d9270a2a2d7fea2b3a398
theschoolofcode <helloschoolofcode@gmail.com>
2021-07-24 19:28:23 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:cloneapps/views/tab_desktop/tab_screen.dart';
+
+import 'shared/app_theme.dart';
+import 'views/tab_desktop/desktop_screen.dart';
+import 'views/mobile/mobile_screen.dart';
+import 'widgets/responsive.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,11 +14,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: 'SwiggyUI',
+      debugShowCheckedModeBanner: false,
+      theme: appPrimaryTheme(),
+      home: Responsive(
+        mobile: MobileScreen(),
+        tablet: TabScreen(),
+        desktop: DesktopScreen(),
       ),
-      home: Container()
     );
   }
 }
